b308f1deff20cced4e76491389744445f3614ff6
KevinAlves55 <programador.kevin4773@gmail.com>
2022-12-25 10:45:58 -0300

    feat: Adicionando uma env do local da API

---
+++ src/app/env/index.ts
@@ -0,0 +1,6 @@
+export const Env = {
+  /**
+   * Altere de acordo com o caminho na pasta xamp > htdocs > pasta onde se localiza o projeto
+   */
+  URL_BASE: "http://localhost/TesteAlphacode/Back-End/server/contatos",
+};
