5d96644e4aba43e2f8766eca7cd2e98fc33d0a32
KevinAlves55 <programador.kevin4773@gmail.com>
2022-12-27 01:50:37 -0300

    feat: Adicionando biblioteca de responsividade

--- src/app/components/table/Table.tsx
+++ src/app/components/table/Table.tsx
@@ -1,4 +1,4 @@
-import { format } from "date-fns";
+import { useMediaQuery } from "react-responsive";
 
 import { IContact } from "../../App";
 
@@ -15,46 +15,86 @@ interface ITabelaProps {
 }
 
 export const Table: React.FC<ITabelaProps> = ({ contatos, handleDeleteContact, handleEditFood }) => {
+  const mdDown = useMediaQuery({ query: "(max-width: 960px)" });
+  const smDown = useMediaQuery({ query: "(max-width: 768px)" });
+
   return (
     <section className={`${common.container} ${styles.secaoTabelaContatos}`}>
-      <table className={styles.tableContainer}>
-        <thead className={styles.headerTable}>
-          <tr>
-            <td>Nome</td>
-            <td>Data de nascimento</td>
-            <td>E-mail</td>
-            <td>Celular para contato</td>
-            <td>Ações</td>
-          </tr>
-        </thead>
-
-        <tbody className={styles.bodyTable}>
-          {contatos.map(contato => (
-            <tr key={contato.id}>
-              <td>{contato.nome}</td>
-              <td>{new Intl.DateTimeFormat("pt-BR").format(
-                new Date(contato.dataDeNascimento)
-              )}</td>
-              <td>{contato.email}</td>
-              <td>{contato.celular}</td>
-              <td className={styles.imgCell}>
-                <img
-                  src={editar}
-                  alt="Editar"
-                  title="Editar"
-                  onClick={() => handleEditFood(contato)}
-                />
-                <img
-                  src={excluir}
-                  alt="Excluir"
-                  title="Excluir"
-                  onClick={() => handleDeleteContact(contato.id)}
-                />
-              </td>
+      {contatos.length > 0 && !smDown && (
+        <table className={styles.tableContainer}>
+          <thead className={styles.headerTable}>
+            <tr>
+              <td>Nome</td>
+              <td>Data de nascimento</td>
+              <td>E-mail</td>
+              <td>Celular para contato</td>
+              <td>Ações</td>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+
+          <tbody className={styles.bodyTable}>
+            {contatos.map(contato => (
+              <tr key={contato.id}>
+                <td>{contato.nome}</td>
+                <td>{new Intl.DateTimeFormat("pt-BR").format(
+                  new Date(contato.dataDeNascimento)
+                )}</td>
+                <td>{contato.email}</td>
+                <td>{contato.celular}</td>
+                <td className={styles.imgCell}>
+                  <img
+                    src={editar}
+                    alt="Editar"
+                    title="Editar"
+                    onClick={() => handleEditFood(contato)}
+                  />
+                  <img
+                    src={excluir}
+                    alt="Excluir"
+                    title="Excluir"
+                    onClick={() => handleDeleteContact(contato.id)}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+
+      {contatos.length > 0 && smDown && (
+        <table className={styles.tableContainer}>
+          <thead className={styles.headerTable}>
+            <tr>
+              <td>Nome</td>
+              <td>Celular para contato</td>
+              <td>Ações</td>
+            </tr>
+          </thead>
+
+          <tbody className={styles.bodyTable}>
+            {contatos.map(contato => (
+              <tr key={contato.id}>
+                <td>{contato.nome}</td>
+                <td>{contato.celular}</td>
+                <td className={styles.imgCell}>
+                  <img
+                    src={editar}
+                    alt="Editar"
+                    title="Editar"
+                    onClick={() => handleEditFood(contato)}
+                  />
+                  <img
+                    src={excluir}
+                    alt="Excluir"
+                    title="Excluir"
+                    onClick={() => handleDeleteContact(contato.id)}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </section>
   );
 };
